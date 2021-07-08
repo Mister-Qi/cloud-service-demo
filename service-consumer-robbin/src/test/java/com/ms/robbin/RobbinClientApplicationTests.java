28d3f37760baf0341e084e84ae23fdccd94bac36
Qi Qi <qiqiflyer@163.com>
2021-07-08 11:06:26 +0800

    add redis

--- service-consumer-robbin/src/test/java/com/ms/robbin/RobbinClientApplicationTests.java
+++ service-consumer-robbin/src/test/java/com/ms/robbin/RobbinClientApplicationTests.java
@@ -2,7 +2,9 @@ package com.ms.robbin;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -12,5 +14,9 @@ public class RobbinClientApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+
+	@Autowired
+	RedisTemplate redisTemplate;
+
 
 }
